5d21ff326c2fadfbd582838bd5c3de55ec9da7f3
Jonathan Newman <jonathan@overfoc.us>
2014-12-07 08:28:33 -0600

    now using autoRegister in TodoForm viewModel

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,2 @@
-// register the TodoForm viewModel so footwork can use it
-fw.viewModels.register('TodoForm', TodoForm);
-
 // tell footwork to startup and begin binding
 fw.start();
--- scripts/viewModels/TodoForm.js
+++ scripts/viewModels/TodoForm.js
@@ -1,6 +1,11 @@
 var TodoForm = fw.viewModel({
+  namespace: 'TodoForm',
+  autoRegister: true,
   initialize: function() {
+    // The observable which stores the current thingToDo form text
     this.thingToDo = fw.observable();
+
+    // Method/Action we want triggered when the form is submitted
     this.submitForm = function() {
       console.info('TodoForm submit action triggered');
     };
