1d17cf2ad0b0f556e1013f01d24cefe1057e2166
Jonathan Newman <jonathan@overfoc.us>
2014-12-09 07:56:12 -0600

    WIP automaticLoading requirejs conversion

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,6 @@
-// tell footwork to startup and begin binding
-fw.start();
+require(['footwork'],
+  function(fw) {
+    // tell footwork to startup and begin binding
+    fw.start(); 
+  }
+);
--- scripts/viewModels/TodoForm.js
+++ scripts/viewModels/TodoForm.js
@@ -1,18 +1,17 @@
-fw.viewModel({
-  namespace: 'TodoForm',
-  autoRegister: true,
-  initialize: function() {
-    this.thingToDo = fw.observable();
-
-    // create the namespace 'channel' we will use to talk to the TodoList
-    this.todoListNamespace = fw.namespace('TodoList');
-    
-    this.submitForm = function() {
-      // The user wishes to add a todo item, lets tell the TodoList what it is
-      this.todoListNamespace.publish('newItem', this.thingToDo());
-
-      // clear out the text box so the user can type a new entry
-      this.thingToDo('');
-    };
+define(['footwork'],
+  function(fw) {
+    return fw.viewModel({
+      namespace: 'TodoForm',
+      autoRegister: true,
+      initialize: function() {
+        this.thingToDo = fw.observable();
+        this.todoListNamespace = fw.namespace('TodoList');
+        
+        this.submitForm = function() {
+          this.todoListNamespace.publish('newItem', this.thingToDo());
+          this.thingToDo('');
+        };
+      }
+    });    
   }
-});
+);
