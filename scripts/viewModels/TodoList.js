92c4fb9139a89e00e62995964a1c20d9323b998b
Jonathan Newman <jonathan@overfoc.us>
2014-12-13 15:24:16 -0600

    added comments to TodoList.js

--- scripts/viewModels/TodoList.js
+++ scripts/viewModels/TodoList.js
@@ -2,8 +2,10 @@ fw.viewModel({
   namespace: 'TodoList',
   autoRegister: true,
   initialize: function() {
-    var self = this;
+    var self = this; // create a reference to this viewModel we can use inside of the callback
     this.todos = fw.observableArray();
+
+    // listen for any 'newItem' messages broadcast on our namespace.
     this.$namespace.subscribe('newItem', function(thingToDo) {
       self.todos.push( new TodoItem(thingToDo) );
     });
