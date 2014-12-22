4824739fdc17f98e5ffb736012a78db92f1a6e86
Jonathan Newman <jonathan@overfoc.us>
2014-12-22 09:18:32 -0600

    more consistency fixes for router refactor

--- scripts/viewModels/LoginForm.js
+++ scripts/viewModels/LoginForm.js
@@ -5,14 +5,14 @@ define(['footwork'],
       initialize: function() {
         /**
          * Create a new namespace instance we will use to
-         * communicate with the Main viewModel.
+         * communicate with the MainRouter.
          */
         this.mainRouterNamespace = fw.namespace('MainRouter');
 
         this.username = fw.observable();
         this.password = fw.observable();
         this.loginUser = function() {
-          // Tell the Main viewModel what user has logged in
+          // Tell the MainRouter that a user has logged in
           this.mainRouterNamespace.trigger('userLogin');
         };
       }
