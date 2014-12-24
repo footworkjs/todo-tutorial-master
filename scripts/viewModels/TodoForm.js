c8bf1b55d42bcf92ee281c35df0f76fb94e5ed69
Jonathan Newman <jonathan@overfoc.us>
2014-12-23 19:25:30 -0600

    fixed TodoForm.js

--- scripts/viewModels/TodoForm.js
+++ scripts/viewModels/TodoForm.js
@@ -2,11 +2,12 @@ var TodoForm = fw.viewModel({
   namespace: 'TodoForm',
   autoRegister: true,
   initialize: function() {
+    // The observable which stores the current thingToDo form text
     this.thingToDo = fw.observable();
 
     // create the namespace 'channel' we will use to talk to the TodoList
     this.todoListNamespace = fw.namespace('TodoList');
-    
+
     this.submitForm = function() {
       // The user wishes to add a todo item, lets tell the TodoList what it is
       this.todoListNamespace.publish('newItem', this.thingToDo());
