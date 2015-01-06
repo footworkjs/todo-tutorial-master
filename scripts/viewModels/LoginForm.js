5fa12cae5d73c96da3ea5182c830c0bc811426cc
Jonathan Newman <jonathan@overfoc.us>
2015-01-05 20:08:37 -0600

    reverted LoginForm.js

--- scripts/viewModels/LoginForm.js
+++ scripts/viewModels/LoginForm.js
@@ -3,17 +3,10 @@ define(['footwork'],
     return fw.viewModel({
       namespace: 'LoginForm',
       initialize: function() {
-        /**
-         * Create a new namespace instance we will use to
-         * communicate with the MainRouter.
-         */
-        this.mainRouterNamespace = fw.namespace('MainRouter');
-
         this.username = fw.observable();
         this.password = fw.observable();
         this.loginUser = function() {
-          // Tell the MainRouter that a user has logged in
-          this.mainRouterNamespace.trigger('userLogin');
+          console.info('Login form submitted!');
         };
       }
     });
