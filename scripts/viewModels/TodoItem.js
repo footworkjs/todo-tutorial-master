92f8dc1e6247b13400b3b1266eb43a15ec90dbf3
Jonathan Newman <jonathan@overfoc.us>
2015-01-10 20:34:00 -0600

    added comment for thingToDo

--- scripts/viewModels/TodoItem.js
+++ scripts/viewModels/TodoItem.js
@@ -3,6 +3,7 @@ define(['footwork'],
     return fw.viewModel({
       namespace: 'TodoItem',
       initialize: function(thingToDo) {
+        // store the string text entered by user as thingToDo
         this.thingToDo = fw.observable(thingToDo);
       }
     });
