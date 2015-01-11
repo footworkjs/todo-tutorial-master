965d43ee5289f8aaa8fdc65d702bfca8781dccc2
Jonathan Newman <jonathan@overfoc.us>
2015-01-11 08:05:01 -0600

    can now complete a todo, main body width now maxed at 500px

--- scripts/viewModels/TodoItem.js
+++ scripts/viewModels/TodoItem.js
@@ -3,8 +3,17 @@ define(['footwork'],
     return fw.viewModel({
       namespace: 'TodoItem',
       initialize: function(thingToDo) {
+        // store the string text entered by user as thingToDo
         this.thingToDo = fw.observable(thingToDo);
+
+        // store the state of the TodoItem (true = done, false = not done)
+        this.isDone = fw.observable(false);
+
+        // toggle the state of this TodoItem
+        this.toggleDone = function() {
+          this.isDone(!this.isDone());
+        };
       }
-    });    
+    });
   }
 );
