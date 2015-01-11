6dadc2f88070e066e16c5caac43a4a8df9c7e15a
Jonathan Newman <jonathan@overfoc.us>
2015-01-11 17:00:18 -0600

    added code for step

--- scripts/viewModels/TodoList.js
+++ scripts/viewModels/TodoList.js
@@ -5,13 +5,21 @@ define(['footwork', '/scripts/viewModels/TodoItem.js'],
       initialize: function() {
         var self = this; // create a reference to this viewModel we can use inside of the callback
 
-        // store the list of TodoItem entries
+        // stores the list of TodoItem entries
         this.todos = fw.observableArray();
 
         // listen for any 'newItem' messages broadcast on our namespace.
         this.$namespace.subscribe('newItem', function(thingToDo) {
           // new thingToDo was received, lets create a new TodoItem based on it
           self.todos.push( new TodoItem(thingToDo) );
+        });
+
+        // listen for any 'setAllAs' commands broadcast on our namespace.
+        this.$namespace.command.handler('setAllAs', function(doneState) {
+          // when a new doneState is received loop through and set each todo
+          self.todos().forEach(function(todo) {
+            todo.isDone(doneState);
+          });
         });
       }
     });
