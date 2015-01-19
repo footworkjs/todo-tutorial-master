5ffb0b9bd0cff1163d23281436c0ddab4591ddac
Jonathan Newman <jonathan@overfoc.us>
2015-01-19 08:15:01 -0600

    updated to latest

--- scripts/app.js
+++ scripts/app.js
@@ -3,31 +3,32 @@
  * footwork are located.
  */
 requirejs.config({
+  baseUrl: '/',
   paths: {
-    "text": "/bower_components/requirejs-text/text",
-    "footwork": "/bower_components/footwork/dist/footwork-all"
+    "text": "bower_components/requirejs-text/text",
+    "footwork": "bower_components/footwork/dist/footwork-all"
   }
 });
 
 require(['footwork'],
   function(fw) {
     // Tell footwork where it can find the TodoList viewModel.
-    fw.viewModels.registerLocation('TodoList', '/scripts/viewModels/');
+    fw.viewModels.registerLocation('TodoList', 'scripts/viewModels/');
 
     /**
      * Here we provide the entire filename to footwork because the lowercase
      * 'todoform' does not match the file names on disk.
      */
     fw.components.registerLocation('todoform', {
-      viewModel: '/scripts/viewModels/TodoForm.js',
-      template: '/scripts/templates/TodoForm.html'
+      viewModel: 'scripts/viewModels/TodoForm', // loads scripts/viewModels/TodoForm.js
+      template: 'scripts/templates/TodoForm' // loads scripts/templates/TodoForm.html
     });
 
     /**
      * Register the location of the 'MainRouter'
-     * Loads from /scripts/MainRouter.js
+     * Loads from scripts/MainRouter.js
      */
-    fw.routers.registerLocation('MainRouter', '/scripts/');
+    fw.routers.registerLocation('MainRouter', 'scripts/');
 
     // tell footwork to startup and begin binding
     fw.start();
