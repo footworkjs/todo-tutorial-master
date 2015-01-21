8e03b98b526bbd7f6922aa4446d780facd6e0e2b
Jonathan Newman <jonathan@overfoc.us>
2015-01-20 19:48:53 -0600

    updated to latest

--- scripts/app.js
+++ scripts/app.js
@@ -3,9 +3,10 @@
  * footwork are located.
  */
 requirejs.config({
+  baseUrl: '/',
   paths: {
-    "text": "/bower_components/requirejs-text/text",
-    "footwork": "/bower_components/footwork/dist/footwork-all"
+    "text": "bower_components/requirejs-text/text",
+    "footwork": "bower_components/footwork/dist/footwork-all"
   }
 });
 
@@ -13,27 +14,27 @@ require(['footwork'],
   function(fw) {
     /**
      * Tell footwork it can find the 'LoginForm' and 'TodoList' viewModels
-     * inside of /scripts/viewModels.
+     * inside of scripts/viewModels.
      */
     fw.viewModels.registerLocation([
-      'LoginForm', // will load from /scripts/viewModels/LoginForm.js
-      'TodoList' // will load from /scripts/viewModels/TodoList.js
-    ], '/scripts/viewModels/');
+      'LoginForm', // will load from scripts/viewModels/LoginForm.js
+      'TodoList' // will load from scripts/viewModels/TodoList.js
+    ], 'scripts/viewModels/');
 
     /**
      * Here we provide the entire filename to footwork because the lowercase
      * 'todoform' does not match the file names on disk.
      */
     fw.components.registerLocation('todoform', {
-      viewModel: '/scripts/viewModels/TodoForm.js',
-      template: '/scripts/templates/TodoForm.html'
+      viewModel: 'scripts/viewModels/TodoForm', // loads scripts/viewModels/TodoForm.js
+      template: 'scripts/templates/TodoForm' // loads scripts/templates/TodoForm.html
     });
 
     /**
      * Register the location of the 'MainRouter'
-     * Loads from /scripts/MainRouter.js
+     * Loads from scripts/MainRouter.js
      */
-    fw.routers.registerLocation('MainRouter', '/scripts/');
+    fw.routers.registerLocation('MainRouter', 'scripts/');
 
     // tell footwork to startup and begin binding
     fw.start();
