74f5865c6f8075c3ecc9bd110dfa7a4e58ae89e3
jnewman <jonathan_newman@legalaccessplans.com>
2015-11-29 17:53:45 -0600

    updated for latest footwork

--- scripts/viewModels/TodoForm.js
+++ scripts/viewModels/TodoForm.js
@@ -1,6 +1,6 @@
 define(['footwork'],
   function(fw) {
-    return fw.viewModel({
+    return fw.viewModel.create({
       namespace: 'TodoForm',
       autoRegister: true,
       initialize: function() {
